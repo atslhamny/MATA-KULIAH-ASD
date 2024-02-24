32a7ae170d5a96c6db69d3d268ff0213c471e0ab
atslhamny <atsilahamanyph@gmail.com>
2024-02-24 16:18:05 +0700

    modify: code

--- FILE JOBSHEET/00 FILE JAVA JOBSHEET/02JBS_ASD/BukuMain04.java
+++ FILE JOBSHEET/00 FILE JAVA JOBSHEET/02JBS_ASD/BukuMain04.java
@@ -2,9 +2,6 @@
  * Buku04
  */
 public class BukuMain04 {
-    String judul, pengarang;
-    int halaman, stok, harga;
-    
     public static void main(String[] args) {
         Buku04 bk1 = new Buku04();
         bk1.judul = "Today Ends Tomorrow Comes";
@@ -25,29 +22,5 @@ public class BukuMain04 {
         Buku04 BukuAtsilah = new Buku04("Love is Illution", "Moy Mori", 200, 12, 100000);
         BukuAtsilah.tampilInformasi();
 
-    }
-
-    void tampilInformasi() {
-        System.out.println("Judul: " + judul);
-        System.out.println("Pengarang: " + pengarang);
-        System.out.println("Jumlah Halaman: " + halaman);
-        System.out.println("Sisa Stok: " + stok);
-        System.out.println("Harga: Rp. " + harga);
-    }
-
-    void terjual(int jml) {
-        if (stok > 0) {
-            stok -= jml;
-        } else {
-            System.out.println("stok tidak valid");
-        }
-    }
-
-    void restock(int jml) {
-        stok += jml;
-    }
-
-    void gantiHarga(int hrg) {
-        harga = hrg;
     }
 }
