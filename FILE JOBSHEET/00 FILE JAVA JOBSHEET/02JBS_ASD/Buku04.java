77d4ce6e24f9d0e86ce33c86ed37d2988c378c1a
atslhamny <atsilahamanyph@gmail.com>
2024-02-24 13:25:18 +0700

    .

---
+++ FILE JOBSHEET/00 FILE JAVA JOBSHEET/02JBS_ASD/Buku04.java
@@ -0,0 +1,29 @@
+/**
+ * Buku04
+ */
+public class Buku04 {
+    String judul, pengarang;
+    int halaman, stok, harga;
+    
+    void tampilInformasi() {
+        System.out.println("Judul: " + judul);
+        System.out.println("Pengarang: " + pengarang);
+        System.out.println("Jumlah Halaman: " + halaman);
+        System.out.println("Sisa Stok: " + stok);
+        System.out.println("Harga: Rp. " + harga);
+    }
+
+    void terjual(int jml){
+        if (stok > 0) {
+            stok -= jml;
+        }else{
+            System.out.println("stok tidak valid");
+        }
+    }
+    void restock(int jml){
+        stok += jml;
+    }
+    void gantiHarga(int hrg){
+        harga = hrg;
+    }
+}
