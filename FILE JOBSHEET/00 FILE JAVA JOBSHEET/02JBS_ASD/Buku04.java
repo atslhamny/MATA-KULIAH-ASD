4a7daa78fb6178eacf5f7ad8e1131ab22446a606
atslhamny <atsilahamanyph@gmail.com>
2024-02-24 16:17:19 +0700

    add: new method

--- FILE JOBSHEET/00 FILE JAVA JOBSHEET/02JBS_ASD/Buku04.java
+++ FILE JOBSHEET/00 FILE JAVA JOBSHEET/02JBS_ASD/Buku04.java
@@ -3,27 +3,60 @@
  */
 public class Buku04 {
     String judul, pengarang;
-    int halaman, stok, harga;
+    int halaman, stok, harga, hargaTotal, diskon, hargaBayar;
     
+    public Buku04(){
+
+    }
+    public Buku04(String jud, String pg, int hal, int stok, int har){
+        judul = jud;
+        pengarang  = pg;
+        halaman = hal;
+        this.stok = stok;
+        harga = har;
+    }
+
     void tampilInformasi() {
         System.out.println("Judul: " + judul);
         System.out.println("Pengarang: " + pengarang);
         System.out.println("Jumlah Halaman: " + halaman);
         System.out.println("Sisa Stok: " + stok);
         System.out.println("Harga: Rp. " + harga);
+        System.out.println("Harga Bayar : Rp. " + hargaBayar);
+       
     }
 
     void terjual(int jml){
         if (stok > 0) {
             stok -= jml;
+            System.out.println(jml + "Buku Tersisa");
         }else{
             System.out.println("stok tidak valid");
         }
+        hitungHargaTotal(jml);
     }
     void restock(int jml){
         stok += jml;
     }
     void gantiHarga(int hrg){
         harga = hrg;
+    }
+
+    void hitungHargaTotal(int jmlBukuTerjual){
+        hargaTotal = harga * jmlBukuTerjual;
+        hitungDiskon();
+    }
+
+    void hitungDiskon(){
+        if (hargaTotal > 150000 ) {
+            diskon = (int) (0.12 * hargaTotal);
+        }else if(hargaTotal > 75000 || hargaTotal < 150000){
+            diskon = (int) (0.05 * hargaTotal);
+        }
+        hitungHargaBayar();
+    }
+    
+    void hitungHargaBayar(){
+        hargaBayar = hargaTotal - diskon;
     }
 }
