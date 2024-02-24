bdabe2213e2222dcc1259dbe31ecd2454d23c3ff
atslhamny <atsilahamanyph@gmail.com>
2024-02-24 14:52:59 +0700

    add: new java file

---
+++ FILE JOBSHEET/00 FILE JAVA JOBSHEET/02JBS_ASD/BukuMain04.java
@@ -0,0 +1,45 @@
+/**
+ * Buku04
+ */
+public class BukuMain04 {
+    String judul, pengarang;
+    int halaman, stok, harga;
+    
+    public static void main(String[] args) {
+        BukuMain04 bk1 = new BukuMain04();
+        bk1.judul = "Today Ends Tomorrow Comes";
+        bk1.pengarang = "Denanda Pratiwi";
+        bk1.halaman = 198;
+        bk1.stok = 13;
+        bk1.harga = 71000;
+
+        bk1.tampilInformasi();
+        bk1.terjual(5);
+        bk1.gantiHarga(60000);
+        bk1.tampilInformasi();
+    }
+
+    void tampilInformasi() {
+        System.out.println("Judul: " + judul);
+        System.out.println("Pengarang: " + pengarang);
+        System.out.println("Jumlah Halaman: " + halaman);
+        System.out.println("Sisa Stok: " + stok);
+        System.out.println("Harga: Rp. " + harga);
+    }
+
+    void terjual(int jml) {
+        if (stok > 0) {
+            stok -= jml;
+        } else {
+            System.out.println("stok tidak valid");
+        }
+    }
+
+    void restock(int jml) {
+        stok += jml;
+    }
+
+    void gantiHarga(int hrg) {
+        harga = hrg;
+    }
+}
