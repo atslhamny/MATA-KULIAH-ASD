9ad825a9992cb58e4b1c4eac5cdae0b4bf2b4a95
atslhamny <atsilahamanyph@gmail.com>
2024-02-28 14:48:25 +0700

    add: tugas teori 3 ASD

---
+++ FILE JOBSHEET/00 FILE JAVA JOBSHEET/03TEORI_ASD/Item04.java
@@ -0,0 +1,26 @@
+/**
+ * Item04
+ */
+public class Item04 {
+
+    public String name;
+    public double price;
+    public int stock;
+
+    public Item04(){
+
+    }
+
+    public Item04(String itemName, double itemPrice, int itemStock){
+        name = itemName;
+        price = itemPrice;
+        stock = itemStock;
+    }
+
+    public void displayInfo(){
+        System.out.println("Name: " + name);
+        System.out.println("Price: " + price);
+        System.out.println("Stock:"  + stock);
+
+    }
+}
